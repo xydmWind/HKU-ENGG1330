e3d33041277cfaa0e49b532535737fff19a50de8
xydmWind <3136556004@qq.com>
2026-09-14 15:11:40 +0800

    Add files via upload

---
+++ C2.py
@@ -0,0 +1,4 @@
+Sec=int(input())
+Min=Sec//60
+Sec=Sec%60
+print(str(Min)+"\n"+str(Sec))
---
+++ C3.py
@@ -0,0 +1,6 @@
+Age=int(input())
+Height=int(input())
+if Age>=18 and Age<=25 and Height>=180:
+    print("Eligible")
+else:
+    print("Not eligible")
